5b8f4d9c13afd48fe9af55549a8be1fc55d0043e
tylim <limteikyee@gmail.com>
2021-10-14 14:49:41 +0800

    test circle ci

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import 'jest'
 import { objSwap } from './index'
+import { expectType } from 'tsd'
 
 describe('objSwap', () => {
 	it('test with {a:"1",b:"2",c:"3"}', () => {
@@ -11,3 +12,7 @@ describe('objSwap', () => {
 		})
 	})
 })
+
+expectType<{ '1': 'a'; '2': 'b'; '3': 'c1' }>(
+	objSwap({ a: '1' as const, b: '2' as const, c: '3' as const })
+)
